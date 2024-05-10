d1abfe5d8276ef406d3a466c875301b1ff513977
HelenIsted <139465011+HelenIsted@users.noreply.github.com>
2024-05-10 11:00:55 +0100

    Added skeleton HTML

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,33 @@
+import React from "react";
+import "./Weather.css";
+
 export default function Weather() {
+  let form = (
+    <form className="searchForm">
+      <input type="search" placeholder="Search a City" />
+      <input type="submit" value="Search" />
+    </form>
+  );
   return (
     <div className="Weather">
-      <button className="btn">Hello</button>
+      {form}
+      <h1>New York</h1>
+      <ul>
+        <li>Date and Time</li>
+        <li>Current weather</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img></img> <p>6^c</p>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation</li>
+            <li>Humidity</li>
+            <li>wind</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }

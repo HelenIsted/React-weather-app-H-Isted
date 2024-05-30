bac7d1808187293a67cfaebe4681f01bdaa1fe32
HelenIsted <139465011+HelenIsted@users.noreply.github.com>
2024-05-30 15:46:04 +0100

    Added new weather icons via a new weather icon component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import FormattedDate from "./FormattedDate.js";
+import WeatherIcon from "./WeatherIcon.js";
 
 export default function WeatherInfo(props) {
+  console.log(props.data.icon.icon);
   return (
     <div>
       {" "}
@@ -16,14 +18,14 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.icon}
-              alt={props.data.description}
-              className="float-left"
-            ></img>
-
-            <span className="temperature">{props.data.temperature}</span>
-            <span className="unit">°C</span>
+            <div className="float-left">
+              <WeatherIcon
+                code={props.data.icon.icon}
+                alt={props.data.icon.description}
+              />
+              <span className="temperature">{props.data.temperature}</span>
+              <span className="unit">°C</span>
+            </div>
           </div>
         </div>
         <div className="col-6">

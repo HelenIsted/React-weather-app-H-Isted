37201ead35c57df7dec9b194176d35b1655dde80
HelenIsted <139465011+HelenIsted@users.noreply.github.com>
2024-05-31 11:33:58 +0100

    Fixed temperature bug

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,7 +6,6 @@ import WeatherTemp from "./WeatherTemp.js";
 export default function WeatherInfo(props) {
   return (
     <div>
-      {" "}
       <h1 className="caps">{props.data.city}</h1>
       <ul>
         <li className="caps">
@@ -14,17 +13,13 @@ export default function WeatherInfo(props) {
         </li>
         <li className="caps">{props.data.description}</li>
       </ul>
-      <div className="row mt-3">
+      <div className="row">
         <div className="col-6">
-          <div className="clearfix">
-            <div className="float-left">
-              <WeatherIcon
-                code={props.data.icon.icon}
-                alt={props.data.icon.description}
-              />
-              <WeatherTemp celsius={props.data.temperature} />
-            </div>
-          </div>
+          <WeatherIcon
+            code={props.data.icon.icon}
+            alt={props.data.icon.description}
+          />
+          <WeatherTemp celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
